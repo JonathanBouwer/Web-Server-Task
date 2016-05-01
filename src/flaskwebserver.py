a70af7dc3aa54c84c7c6d03da0c60b58663db89a
Jonathan Bouwer <BWRJON001@myuct.ac.za>
2016-05-01 20:45:28 +0200

    Added top output to flask webserver, will neaten up output in following commits

--- src/flaskwebserver.py
+++ src/flaskwebserver.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+import subprocess
+import os
+
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
+@app.route("/stats")
+def stats():
+	myenv = os.environ.copy()
+	myenv["COLUMNS"] = "512"
+	s = subprocess.check_output(["top","-b", "-n", "1"], env=myenv)
+	return str(s)
+
+@app.route("/")
+def index():
+	return redirect(url_for('stats'))
+
 
 if __name__ == "__main__":
-    app.run()
+	app.debug=True
+	app.run()
