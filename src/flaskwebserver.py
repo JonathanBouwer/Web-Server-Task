9d7afb799e997dc5587c784b6e173ee8f50d42ae
Jonathan Bouwer <BWRJON001@myuct.ac.za>
2016-05-02 11:45:02 +0200

    Implemented a graph based interface for the webserver stats page using chart.js

--- src/flaskwebserver.py
+++ src/flaskwebserver.py
@@ -6,16 +6,27 @@ app = Flask(__name__)
 
 @app.route("/stats")
 def stats():
-	myenv = os.environ.copy()
-	myenv["COLUMNS"] = "512"
-	topData = subprocess.check_output(["top","-b", "-n", "1"], env=myenv)
-	sInfo = topData
-	return render_template("main.html", sInfo = sInfo)
+	sInfo = getUsageInfo()
+	
+	CPU = "50%"
+	MEM = "60%"
+	l = ["l1","l2","l3","l4","l5","Other"]
+	d = [12,15,17,20,19,100-12-15-17-20-19]
+	return render_template("main.html",CPU=CPU,MEM=MEM,l=l,d=d)
 
 @app.route("/")
 def index():
 	return redirect(url_for('stats'))
 
+@app.route("/servInfo")
+def servInfo():
+	sInfo = getUsageInfo()
+	return render_template("servInfo.html", sInfo = sInfo)
+
+def getUsageInfo():
+	myenv = os.environ.copy()
+	myenv["COLUMNS"] = "512"
+	return subprocess.check_output(["top","-b", "-n", "1"], env=myenv)
 
 if __name__ == "__main__":
 	app.debug=True
